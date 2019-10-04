e3cf7a3a287c7704472bddc1d5723aae270c8a3d
wangdong <20004604@qq.com>
2019-10-04 17:45:32 +0800

    将文章查询系统单独拆分出来，计划使用 RPC 来查询，方便后面解耦和复用

---
+++ article_server/config/mysql_options.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+mysql_config = {
+    'database': 'article_system',
+    'host': '127.0.0.1',
+    'user': 'root',
+    'pw': 'abcd1235ojpibetbenoinwef'
+}
