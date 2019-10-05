be68d39cc5cad3247655fa3b14b6dc98fcd46399
wangdong <20004604@qq.com>
2019-10-05 20:48:06 +0800

    可以使用 RPC 通信新增 article 了

--- article_server/config/mysql_options.py
+++ article_server/config/mysql_options.py
@@ -4,6 +4,7 @@
 mysql_config = {
     'database': 'article_system',
     'host': '127.0.0.1',
+    'port': 55001,
     'user': 'root',
     'pw': 'abcd1235ojpibetbenoinwef'
 }
---
+++ article_server/proto_container/client/__init__.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import time
+import grpc
+
+# 必须先设置一下环境路径
+sys.path.append("../proto")
+
+import article_pb2, article_pb2_grpc
+
+HOST = 'localhost'
+PORT = 55002
+
+
+def error_log(msg):
+    with open('./rpc_err.log', 'a')as f:
+        f.write('%s||%s||%s\n' % (
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            sys._getframe(1).f_code.co_name,  # 执行errlog这个函数的函数名字，即上一级函数
+            msg
+        ))
+
+
+# RPC专用类（客户端）
+class GRPCClient(object):
+    def __init__(self):
+        server = '%s:%s' % (HOST, PORT)
+        # 连接 rpc 服务器
+        channel = grpc.insecure_channel(server)
+        # 调用 rpc 服务，GreeterStub 这个类名是固定生成的，参照 helloworld_pb2_grpc.py
+        self.stub = article_pb2_grpc.ArticleServiceStub(channel)
+
+    def add_article(self, article):
+        is_error = False
+        error_msg = ''
+        try:
+            # s 是一个基于 dict 的实例
+            s = article_pb2.AddArticleRequest(article=article)
+            # 调用 SayHello 向 Server 发送信息
+            # 这个函数是在 __init__ 时被定义的，定义时，添加了3个参数，分别定义了唯一标识、指定压缩格式（str to 二进制）、解压缩格式。
+            # 所以这里调用时，会被自动压缩成对应的参数，然后发送到 server 去。
+            # 如果只看转成二进制字符串这一步，相当于
+            # import grpc._common as common
+            # common.serialize(s, helloworld_pb2.HelloRequest.SerializeToString)
+            # 然后你也可以拿去，用自己的通信方式去发送数据
+            response = self.stub.AddArticle(s)
+        except BaseException as e:
+            # 这个错误信息可能是服务器连接失败
+            is_error = True
+            error_msg = e
+            error_log(e.details())
+            return {
+                'code': 0,
+                'msg': 'send error',
+                'data': None
+            }
+        finally:
+            # 组织返回信息
+            if is_error:
+                return {
+                    'code': 0,
+                    'msg': error_msg,
+                    'data': None
+                }
+            else:
+                if response.code is 200:
+                    return {
+                        'code': 200,
+                        'msg': 'success',
+                        'data': {
+                            'id': response.data.id
+                        }
+                    }
+                else:
+                    return {
+                        'code': 0,
+                        'msg': response.msg,
+                        'data': None
+                    }
+
+
+# 测试和示例代码
+if __name__ == '__main__':
+    client = GRPCClient()
+    result_add = client.add_article("abcdefg")
+    print(result_add)
+
+    # res2 = client.send_people('张三', 20)
+    # if res2['code'] is 200:
+    #     print(res2['msg'])
+    #     print(res2['data'].isRight)
+    # else:
+    #     print('error')
